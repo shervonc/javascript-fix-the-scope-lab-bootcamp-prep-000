305250679db1a29d4bf876c9b0b7a8ebd89fd9de
Von C <shervonc@gmail.com>
2019-07-16 16:30:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = function(){
   return function() {
+    return funkyFunction
   }                       
                     }
                                 }
